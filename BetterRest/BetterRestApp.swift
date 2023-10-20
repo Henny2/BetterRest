be1e129aa77325d821559be372260cdd0780aece
Henny2 <henrieke.baunack@web.de>
2023-10-19 19:55:48 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Henrieke Baunack on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
